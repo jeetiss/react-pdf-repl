6114dd61c2456118399dc546244d83ba6ce88f32
Dmitry Ivakhnenko <jeetiss@yandex.ru>
2022-02-07 16:46:43 +0300

    repl

--- pages/index.js
+++ pages/index.js
@@ -1,175 +1,182 @@
-/* eslint-disable react/display-name */
-import { useEffect } from "react";
-import { useAtom } from "jotai";
-import { Mosaic, MosaicWindow } from "react-mosaic-component";
+import { useEffect, useReducer, useState } from "react";
+import LZString from "lz-string";
 
-import doc from "../code/diff-content";
-import { pdf } from "../exp-layout";
+import { createSingleton } from "../hooks";
+import { WorkerV2 } from "../worker";
 
-import styles from "../styles/layout.module.css";
+import dynamic from "next/dynamic";
+import { code as defCode } from "../code/nth-page";
 
-import ElementsTree from "../components/elements-tree";
-import DebugTree from "../components/debug-tree";
-import BoxSizing from "../components/box-sizing";
+// import definitions from "../types/builded.d.ts";
 
-import {
-  layoutAtom,
-  urlAtom,
-  selectedAtom,
-  mosaicAtom,
-  toggleAtom,
-  pageCountAtom,
-  pageNumberAtom,
-} from "../code/store";
+const compress = (string) =>
+  LZString.compressToBase64(string)
+    .replace(/\+/g, "-") // Convert '+' to '-'
+    .replace(/\//g, "_") // Convert '/' to '_'
+    .replace(/=+$/, ""); // Remove ending '='
+
+const decompress = (string) =>
+  LZString.decompressFromBase64(
+    string
+      .replace(/-/g, "+") // Convert '-' to '+'
+      .replace(/_/g, "/") // Convert '_' to '/'
+  );
 
 import { Document, Page, pdfjs } from "react-pdf";
 import workerSrc from "pdfjs-dist/build/pdf.worker.min.js";
 pdfjs.GlobalWorkerOptions.workerSrc = workerSrc;
 
-const addId = (node, parent, prefix, postfix) => {
-  if (parent) node.parent = parent;
-  node._id = [prefix, node.type, postfix].filter((v) => v).join("__");
+const MonacoEditor = dynamic(import("react-monaco-editor"), { ssr: false });
 
-  if (node.children)
-    node.children.forEach((child, index) => {
-      addId(child, node, node._id, index + 1);
-    });
+const useWorkerV2 = createSingleton(
+  () => new WorkerV2(),
+  (worker) => worker.terminate()
+);
 
-  return node;
-};
+const useMergeState = (initial) =>
+  useReducer((s, a) => ({ ...s, ...a }), initial);
 
-const Preview = ({ path }) => {
-  const [debuggerDisabled, toggle] = useAtom(toggleAtom);
-  const [layout] = useAtom(layoutAtom);
-  const [url] = useAtom(urlAtom);
+const Repl = () => {
+  const [state2, update2] = useMergeState({
+    url: null,
+    version: null,
+    time: null,
+  });
 
-  const [count, setCount] = useAtom(pageCountAtom);
-  const [page, setPage] = useAtom(pageNumberAtom);
+  const [page, setPage] = useState(1);
+  const [code, setCode] = useState(() => {
+    if (typeof window === "undefined") return;
+
+    const query = new URLSearchParams(window.location.search);
+    if (query.has("code")) {
+      return decompress(query.get("code"));
+    }
+
+    return defCode;
+  });
+
+  const pdfV2 = useWorkerV2();
+
+  useEffect(() => {
+    pdfV2.call("version").then((version) => update2({ version }));
+  }, [pdfV2, update2]);
+
+  useEffect(() => {
+    const startTime = Date.now();
+    pdfV2
+      .call("evaluate", code)
+      .then((url) => update2({ url, time: Date.now() - startTime }));
+  }, [pdfV2, code, update2]);
 
   return (
-    <MosaicWindow
-      path={path}
-      renderToolbar={() => (
+    <div style={{ display: "flex" }}>
+      <MonacoEditor
+        width="50%"
+        height="100vh"
+        value={code}
+        onChange={setCode}
+        language="javascript"
+        editorWillMount={(monaco) => {
+          monaco.languages.typescript.typescriptDefaults.setCompilerOptions({
+            jsx: monaco.languages.typescript.JsxEmit.React,
+            lib: [],
+          });
+
+          // const fakeDefs = [
+          //   "declare const render: (arg: any) => void;",
+          //   "declare const React: object;",
+          // ].join("\n");
+
+          // const fakeUri = "ts:filename/a.d.ts";
+
+          // monaco.languages.typescript.typescriptDefaults.addExtraLib(
+          //   fakeDefs,
+          //   fakeUri
+          // );
+
+          // monaco.editor.createModel(
+          //   fakeDefs,
+          //   "typescript",
+          //   monaco.Uri.parse(fakeUri)
+          // );
+
+          let model = monaco.editor.createModel(
+            code,
+            "javascript",
+            monaco.Uri.file("foo.jsx")
+          );
+          return { model };
+        }}
+        editorDidMount={(editor, monaco) => {
+          // console.log(editor, monaco);
+          // monaco.editor.onDidChangeMarkers((uris) => {
+          //   const editorUri = editor.getModel().uri;
+          //   if (editorUri) {
+          //     const currentEditorHasMarkerChanges = uris.find(
+          //       (uri) => uri.path === editorUri.path
+          //     );
+          //     if (currentEditorHasMarkerChanges) {
+          //       const markers = monaco.editor.getModelMarkers({
+          //         resource: editorUri,
+          //       });
+          //       console.log(markers);
+          //     }
+          //   }
+          // });
+        }}
+      />
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            width: "100%",
+            justifyContent: "space-around",
+          }}
+        >
+          <div style={{ textAlign: "right" }}>
+            <div>react-pdf v{state2.version}</div>
+            <div>time:{state2.time}</div>
+          </div>
+        </div>
+
+        {state2.url && (
+          <Document file={state2.url}>
+            <Page
+              scale={0.85}
+              pageNumber={page}
+              renderTextLayer={false}
+              renderAnnotationLayer={false}
+            />
+          </Document>
+        )}
+
         <div>
-          <button onClick={toggle}>debugger;</button>
-          <a
-            href={url}
-            target="_blank"
-            rel="noreferrer"
-            download="document.pdf"
+          <button onClick={() => setPage((page) => page - 1)}>prev</button>
+          {page}
+          <button onClick={() => setPage((page) => page + 1)}>next</button>
+        </div>
+
+        <div>
+          <button
+            onClick={() => {
+              const link = new URL(window.location);
+              link.search = `?code=${compress(code)}`;
+              navigator.clipboard.writeText(link.toString());
+            }}
           >
-            Download
-          </a>
-        </div>
-      )}
-    >
-      {layout && (
-        <div className={styles.previewContainer}>
-          <div className={styles.preview}>
-            <Document
-              file={url}
-              onLoadSuccess={({ numPages }) => setCount(numPages)}
-            >
-              <Page
-                pageNumber={page}
-                renderAnnotationLayer={false}
-                renderTextLayer={false}
-              />
-            </Document>
-
-            {debuggerDisabled || (
-              <div className={styles.debug}>
-                <DebugTree nodes={[layout]}></DebugTree>
-              </div>
-            )}
-          </div>
-
-          <div>
-            <button onClick={() => setPage(page - 1)}>{"<"}</button>
-            {page} - {count}
-            <button onClick={() => setPage(page + 1)}>{">"}</button>
-          </div>
-        </div>
-      )}
-    </MosaicWindow>
-  );
-};
-
-const Debugger = ({ path }) => {
-  const [selected] = useAtom(selectedAtom);
-  const [, toggle] = useAtom(toggleAtom);
-  const [layout] = useAtom(layoutAtom);
-
-  return (
-    <MosaicWindow
-      path={path}
-      toolbarControls={[
-        <button key="1" onClick={toggle}>
-          Close
-        </button>,
-      ]}
-    >
-      <div>
-        <div className={styles.code}>
-          {layout && <ElementsTree nodes={[layout]} />}
-        </div>
-
-        <div className={styles.details}>
-          <div>computed</div>
-
-          <div>
-            {selected &&
-              Object.entries(selected.style).map(([style, value]) => (
-                <div key={style}>
-                  {style}: {value}
-                </div>
-              ))}
-          </div>
-
-          <BoxSizing box={selected ? selected.box : {}} />
+            copy link
+          </button>
         </div>
       </div>
-    </MosaicWindow>
-  );
-};
-
-const ELEMENT_MAP = {
-  preview: Preview,
-  debugger: Debugger,
-};
-
-export default function Home() {
-  const [currentNode, setNode] = useAtom(mosaicAtom);
-
-  const [layout, setLayout] = useAtom(layoutAtom);
-  const [, setUrl] = useAtom(urlAtom);
-
-  useEffect(() => {
-    const { component: Doc } = doc;
-    const instance = pdf();
-
-    instance.updateContainer(<Doc />);
-    instance.toBlob().then((blob) => {
-      setUrl(URL.createObjectURL(blob));
-      setLayout(addId(blob.__SECRET_LAYOUT_DO_NOT_USE_OR_YOU_WILL_BE_FIRED));
-    });
-  }, [setLayout, setUrl]);
-
-  useEffect(() => {
-    console.log(layout);
-  }, [layout]);
-
-  return (
-    <div className={styles.container}>
-      <Mosaic
-        renderTile={(id, path) => {
-          const Component = ELEMENT_MAP[id];
-          return <Component path={path} />;
-        }}
-        value={currentNode}
-        onChange={setNode}
-        className=""
-      />
     </div>
   );
-}
+};
+
+export default Repl;
