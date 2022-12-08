ad68cab9f1624d7c58412774e2858be8d09dffc8
Dmitry Ivakhnenko <jeetiss@yandex.ru>
2022-12-08 12:27:20 +0300

    setup analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,15 @@
+import { Analytics } from "@vercel/analytics/react";
+
 import "../styles/globals.css";
 import "../styles/mosaic.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />
+      <Analytics />
+    </>
+  );
 }
 
 export default MyApp;
