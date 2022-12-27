4ec26d27c7d9cdd191124004ad182b511792453d
Dmitry Ivakhnenko <jeetiss@yandex.ru>
2022-12-28 00:19:41 +0300

    fix safari 🥰

--- pages/index.js
+++ pages/index.js
@@ -87,10 +87,18 @@ function useMediaQuery(query) {
     // Triggered at the first client-side load and if query changes
     handleChange();
 
-    matchMedia.addEventListener("change", handleChange);
+    if (matchMedia.addListener) {
+      matchMedia.addListener(handleChange);
+    } else {
+      matchMedia.addEventListener("change", handleChange);
+    }
 
     return () => {
-      matchMedia.removeEventListener("change", handleChange);
+      if (matchMedia.removeListener) {
+        matchMedia.removeListener(handleChange);
+      } else {
+        matchMedia.removeEventListener("change", handleChange);
+      }
     };
   }, [query]);
 
