dd2d41d6d212ba2e2bd707eb3444a5757e56c92a
Dmitry Ivakhnenko <jeetiss@yandex.ru>
2023-01-28 17:13:33 +0300

    use only minor versions, add new react-pdf with gaps

--- worker/multi-source.js
+++ worker/multi-source.js
@@ -135,15 +135,11 @@ const reactRuntimeModule = createVirtualModuleFromVariable(
 const versions = {
   "1.6.17": wrap(() => import("rpr1.6.17")),
   "2.0.21": wrap(() => import("rpr2.0.21")),
-  "2.1.0": wrap(() => import("rpr2.1.0")),
-  "2.1.1": wrap(() => import("rpr2.1.1")),
   "2.1.2": wrap(() => import("rpr2.1.2")),
   "2.2.0": wrap(() => import("rpr2.2.0")),
   "2.3.0": wrap(() => import("rpr2.3.0")),
-  "3.0.0": wrap(() => import("rpr3.0.0")),
-  "3.0.1": wrap(() => import("rpr3.0.1")),
-  "3.0.2": wrap(() => import("rpr3.0.2")),
-  "3.0.3": wrap(() => import("@react-pdf/renderer")),
+  "3.0.3": wrap(() => import("rpr3.0.3")),
+  "3.1.1": wrap(() => import("@react-pdf/renderer")),
 };
 
 const isRelative = (spec) =>
