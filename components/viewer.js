f58b49a0e00d06ed6a8f2277e1319b49979a9d43
Dmitry Ivakhnenko <jeetiss@yandex.ru>
2023-02-15 01:11:03 +0300

    fix loader and view for empty document

--- components/viewer.js
+++ components/viewer.js
@@ -195,30 +195,32 @@ const Viewer = ({ page: pageNumber, url, isDebugging, layout, onParse }) => {
         {isDebugging && layout && (
           <TreeCore nodes={[layout]} size={state.size} />
         )}
-
-        {state.document && !pageNumber && (
-          <div
-            style={{
-              position: "absolute",
-              top: "50%",
-              left: "50%",
-              transform: "translate(-50%, -50%)",
-            }}
-          >
-            Blank PDF Document
-          </div>
-        )}
-
-        {state.size && !state.document && (
-          <div
-            className={loader}
-            style={{
-              width: "100%",
-              height: "100%",
-            }}
-          />
-        )}
       </div>
+
+      {state.document && !pageNumber && (
+        <div
+          style={{
+            position: "absolute",
+            top: "50%",
+            left: "50%",
+            transform: "translate(-50%, -50%)",
+          }}
+        >
+          Blank PDF Document
+        </div>
+      )}
+
+      {state.size && !state.document && (
+        <div
+          className={loader}
+          style={{
+            width: "100%",
+            minWidth: 200,
+            maxWidth: 900,
+            height: "100%",
+          }}
+        />
+      )}
     </div>
   );
 };
