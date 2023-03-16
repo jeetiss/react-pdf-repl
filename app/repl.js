95161752b4f84eadcecb696c1f0fc67df2067dad
Dmitry Ivakhnenko <jeetiss@yandex.ru>
2023-03-17 00:22:53 +0300

    dynamic og image (#10)
    * dynamic og image
    
    * try to fix
    
    * bundle worker
    
    * use @napi-rs/canvas instead of canvas
    
    * update lock
    
    * import instead of url
    
    * set cache
    
    * default code
    
    * setup dynamic og images
    
    * remove unused code

---
+++ app/repl.js
@@ -0,0 +1,468 @@
+"use client";
+
+import { Suspense, lazy, useEffect, useReducer, useRef, useState } from "react";
+import Editor from "@monaco-editor/react";
+import LZString from "lz-string";
+import useConstant from "use-constant";
+import { useAtom } from "jotai/react";
+import { log } from "next-axiom/dist/logger";
+
+import { Panel as ResizablePanel, PanelGroup } from "react-resizable-panels";
+
+import { createSingleton, useSetState } from "../hooks";
+import { Worker } from "../worker";
+import Tree from "../components/elements-tree";
+import BoxSizing from "../components/box-sizing";
+import {
+  Buttons,
+  Version,
+  ResizeHandle,
+  ScrollBox,
+  DebugFont,
+  DebugInfo,
+  Styles,
+  BoxInfo,
+  PreviewPanel,
+  HeaderControls,
+  FooterControls,
+  EmptyDebugger,
+  Preview,
+  Error,
+  GithubButton,
+} from "../components/repl-layout";
+import { loader } from "../components/viewer.module.css";
+import {
+  page,
+  pagesCount,
+  canDecrease,
+  canIncrease,
+  increase,
+  decrease,
+} from "../state/page";
+
+import { layoutAtom, selectedAtom } from "../state/debugger";
+
+import { code as defCode } from "../code/default-example";
+
+const Viewer = lazy(() => import("../components/viewer"));
+
+const compress = (string) =>
+  LZString.compressToBase64(string)
+    .replace(/\+/g, "-") // Convert '+' to '-'
+    .replace(/\//g, "_") // Convert '/' to '_'
+    .replace(/=+$/, ""); // Remove ending '='
+
+const decompress = (string) =>
+  LZString.decompressFromBase64(
+    string
+      .replace(/-/g, "+") // Convert '-' to '+'
+      .replace(/_/g, "/") // Convert '_' to '/'
+  );
+
+const useWorker = createSingleton(
+  () => (typeof document !== "undefined" ? new Worker() : null),
+  (worker) => worker?.terminate()
+);
+
+function useMediaQuery(query) {
+  const getMatches = (query) => {
+    if (typeof window !== "undefined") {
+      return window.matchMedia(query).matches;
+    }
+    return false;
+  };
+
+  const [matches, setMatches] = useState(() => getMatches(query));
+
+  useEffect(() => {
+    function handleChange() {
+      setMatches(getMatches(query));
+    }
+
+    const matchMedia = window.matchMedia(query);
+
+    // Triggered at the first client-side load and if query changes
+    handleChange();
+
+    matchMedia.addEventListener("change", handleChange);
+
+    return () => {
+      matchMedia.removeEventListener("change", handleChange);
+    };
+  }, [query]);
+
+  return matches;
+}
+
+const ClientOnly = ({ children }) => {
+  const [isClient, set] = useState(false);
+
+  useEffect(() => set(true), []);
+
+  return isClient ? children : null;
+};
+
+const Loader = () => <div className={loader} />;
+
+const addId = (node, parent, prefix, postfix) => {
+  if (parent) node.parent = parent;
+  node._id = [prefix, node.type, postfix].filter((v) => v).join("__");
+
+  if (node.children)
+    node.children.forEach((child, index) => {
+      addId(child, node, node._id, index + 1);
+    });
+
+  return node;
+};
+
+const createLink = (options) => {
+  const link = new URL(window.location);
+
+  link.searchParams.set("cp_code", compress(options.code));
+
+  if (options.modules) {
+    link.searchParams.set("modules", options.modules);
+  }
+
+  return link.toString();
+};
+
+const Repl = () => {
+  const urlParams = useConstant(() => {
+    if (typeof window === "undefined") return {};
+
+    return Object.fromEntries(
+      Array.from(new URLSearchParams(window.location.search).entries()).map(
+        ([key, value]) => {
+          if (key.startsWith("cp_")) {
+            return [key.slice(3), decompress(value)];
+          }
+
+          return [key, value];
+        }
+      )
+    );
+  });
+
+  const isMobile = useMediaQuery("(max-width: 600px)");
+
+  const [options, updateOptions] = useSetState(() => ({
+    modules: urlParams.code ? Boolean(urlParams.modules) : true,
+  }));
+
+  const timeout = useRef(20_000);
+
+  const [state, update] = useSetState(() => ({
+    url: null,
+    version: null,
+    time: null,
+    error: null,
+    isDebuggingSupported: options.modules,
+    isDebugging: true,
+    isEditing: true,
+  }));
+
+  const [isReady, setReady] = useState(false);
+  const [code, setCode] = useState(() => urlParams.code ?? defCode);
+  const [v, forceRender] = useReducer((v) => !v);
+
+  const [pageV] = useAtom(page);
+  const [, setPagesCount] = useAtom(pagesCount);
+  const [canDecreaseV] = useAtom(canDecrease);
+  const [, decreaseS] = useAtom(decrease);
+  const [canIncreaseV] = useAtom(canIncrease);
+  const [, increaseS] = useAtom(increase);
+
+  const [layout, setLayout] = useAtom(layoutAtom);
+  const [selectedNode] = useAtom(selectedAtom);
+
+  const pdf = useWorker();
+
+  const debuggerAPI = useRef();
+  const editorPanelAPI = useRef();
+
+  useEffect(() => {
+    if (isReady) {
+      pdf.call("version").then(({ version, isDebuggingSupported }) =>
+        update({
+          version,
+          isDebuggingSupported: options.modules && isDebuggingSupported,
+        })
+      );
+    } else {
+      pdf.call("init").then(() => setReady(true));
+    }
+  }, [pdf, update, isReady, options.modules]);
+
+  useEffect(() => {
+    if (isReady) {
+      const startTime = Date.now();
+      pdf
+        .call("evaluate", {
+          code,
+          options: { modules: options.modules },
+          timeout: timeout.current,
+        })
+        .then(({ url, layout }) => {
+          if (layout) {
+            setLayout(addId(layout));
+          } else {
+            setLayout(null);
+          }
+          update({ url, time: Date.now() - startTime, error: null });
+        })
+        .catch((error) => {
+          if (error.fatal) {
+            log.error(error.message, {
+              link: createLink({ code, ...options }),
+            });
+          }
+          update({ time: Date.now() - startTime, error });
+        });
+    }
+  }, [pdf, code, update, isReady, setLayout, options, v]);
+
+  const editorPanel = (
+    <ResizablePanel
+      ref={editorPanelAPI}
+      defaultSize={50}
+      minSize={20}
+      collapsible
+      onCollapse={(collapsed) => update({ isEditing: !collapsed })}
+    >
+      <Editor
+        loading={<Loader />}
+        language="javascript"
+        value={code}
+        onChange={(newCode) => {
+          setCode(newCode ?? "");
+        }}
+        beforeMount={(_monaco) => {
+          _monaco.languages.typescript.javascriptDefaults.setCompilerOptions({
+            allowNonTsExtensions: true,
+            checkJs: true,
+            allowJs: true,
+            noLib: true,
+            jsx: "react",
+          });
+          _monaco.languages.typescript.javascriptDefaults.setDiagnosticsOptions(
+            { noSemanticValidation: true }
+          );
+        }}
+        options={{
+          wordWrap: "on",
+          tabSize: 2,
+          minimap: {
+            enabled: false,
+          },
+          contextmenu: false,
+        }}
+      />
+    </ResizablePanel>
+  );
+
+  const viewerPanel = (
+    <ResizablePanel minSize={20}>
+      <PanelGroup autoSaveId="react-pdf-repl-debug" direction="vertical">
+        <ResizablePanel minSize={20}>
+          <PreviewPanel>
+            <HeaderControls>
+              <Version time={state.time} value={state.version} />
+
+              {pageV && (
+                <div style={{ display: "flex", alignItems: "center" }}>
+                  <button disabled={!canDecreaseV} onClick={() => decreaseS()}>
+                    {"<"}
+                  </button>
+                  <div
+                    style={{
+                      display: "flex",
+                      alignItems: "center",
+                      padding: "0px 0px 3px 5px",
+                    }}
+                  >
+                    page:
+                    <div style={{ textAlign: "center", minWidth: 20 }}>
+                      {pageV}
+                    </div>
+                  </div>
+                  <button disabled={!canIncreaseV} onClick={() => increaseS()}>
+                    {">"}
+                  </button>
+                </div>
+              )}
+
+              <Buttons>
+                <button
+                  onClick={() =>
+                    navigator.clipboard.writeText(
+                      createLink({ code, ...options })
+                    )
+                  }
+                >
+                  copy link
+                </button>
+
+                <button
+                  onClick={() => {
+                    window.open(state.url);
+                  }}
+                >
+                  open pdf
+                </button>
+
+                <GithubButton />
+              </Buttons>
+            </HeaderControls>
+
+            <Preview style={{ overflow: state.error ? "hidden" : "scroll" }}>
+              <ScrollBox>
+                <Viewer
+                  url={state.url}
+                  page={pageV}
+                  isDebugging={state.isDebugging}
+                  layout={layout}
+                  onParse={({ pagesCount }) => setPagesCount(pagesCount)}
+                />
+              </ScrollBox>
+
+              {state.error && (
+                <Error
+                  error={state.error}
+                  actions={[
+                    [
+                      "restart",
+                      () => {
+                        pdf.start();
+                        forceRender();
+                      },
+                    ],
+
+                    [
+                      "increase timeout and restart",
+                      () => {
+                        pdf.start();
+                        timeout.current = timeout.current * 2;
+                        forceRender();
+                      },
+                    ],
+                  ]}
+                />
+              )}
+            </Preview>
+
+            <FooterControls>
+              <button
+                onClick={() => {
+                  const panel = editorPanelAPI.current;
+                  if (panel) {
+                    if (state.isEditing) {
+                      panel.collapse();
+                    } else {
+                      panel.expand();
+                    }
+                  }
+                }}
+              >
+                {state.isEditing ? "hide" : "show"} editor
+              </button>
+              <button
+                onClick={() => {
+                  const panel = debuggerAPI.current;
+                  if (panel) {
+                    if (state.isDebugging) {
+                      panel.collapse();
+                    } else {
+                      panel.expand();
+                    }
+                  }
+                }}
+              >
+                {state.isDebugging ? "hide" : "show"} debugger
+              </button>
+            </FooterControls>
+          </PreviewPanel>
+        </ResizablePanel>
+
+        <ResizeHandle />
+
+        <ResizablePanel
+          minSize={20}
+          collapsible
+          onCollapse={(collapsed) => update({ isDebugging: !collapsed })}
+          ref={debuggerAPI}
+        >
+          {state.isDebuggingSupported ? (
+            <PanelGroup
+              autoSaveId="react-pdf-repl-debug-info"
+              direction="horizontal"
+            >
+              <ResizablePanel>
+                {layout && (
+                  <ScrollBox>
+                    <DebugFont>
+                      <Tree nodes={[layout]} />
+                    </DebugFont>
+                  </ScrollBox>
+                )}
+              </ResizablePanel>
+              <ResizeHandle />
+              <ResizablePanel>
+                <ScrollBox>
+                  <DebugInfo>
+                    {selectedNode && selectedNode.style && (
+                      <Styles>
+                        <pre>
+                          {Object.entries(selectedNode.style)
+                            .map(([key, value]) => `${key}: ${value}`)
+                            .join("\n")}
+                        </pre>
+                      </Styles>
+                    )}
+
+                    {selectedNode && (
+                      <BoxInfo>
+                        <BoxSizing box={selectedNode.box} />
+                      </BoxInfo>
+                    )}
+                  </DebugInfo>
+                </ScrollBox>
+              </ResizablePanel>
+            </PanelGroup>
+          ) : (
+            <EmptyDebugger>{`Debugger doesn't supported by this @react-pdf/renderer version`}</EmptyDebugger>
+          )}
+        </ResizablePanel>
+      </PanelGroup>
+    </ResizablePanel>
+  );
+
+  return (
+    <ClientOnly>
+      {isMobile ? (
+        <PanelGroup
+          key="mobile"
+          autoSaveId="react-pdf-repl-mobile"
+          direction="vertical"
+        >
+          {viewerPanel}
+          <ResizeHandle />
+          {editorPanel}
+        </PanelGroup>
+      ) : (
+        <PanelGroup
+          key="desktop"
+          autoSaveId="react-pdf-repl"
+          direction="horizontal"
+        >
+          {editorPanel}
+          <ResizeHandle />
+          {viewerPanel}
+        </PanelGroup>
+      )}
+    </ClientOnly>
+  );
+};
+
+export default Repl;
---
+++ pages/api/og.js
@@ -0,0 +1,373 @@
+import "ses";
+import { createCanvas, Image } from "@napi-rs/canvas";
+import * as pdfjs from "pdfjs-dist/legacy/build/pdf";
+import "pdfjs-dist/legacy/build/pdf.worker";
+import { readFile } from "node:fs/promises";
+import LZString from "lz-string";
+
+import * as React from "react";
+import { jsx, jsxs, Fragment } from "react/jsx-runtime";
+import * as tailwind from "react-pdf-tailwind";
+import * as rpGlobals from "@react-pdf/renderer";
+
+import { StaticModuleRecord } from "../../worker/better-static-module-record.mjs";
+import { code } from "../../code/default-example";
+
+const canvas = createCanvas(1600, 900);
+
+const templatePromise = readFile(
+  new URL("../../public/template.png", import.meta.url)
+);
+
+const decompress = (string) =>
+  LZString.decompressFromBase64(
+    string
+      .replace(/-/g, "+") // Convert '-' to '+'
+      .replace(/_/g, "/") // Convert '_' to '/'
+  );
+
+const getTemplate = async () => {
+  const template = new Image();
+  template.src = await templatePromise;
+
+  return template;
+};
+
+const makeStaticRetriever = (sources) => {
+  return async (moduleLocation) => {
+    const string = sources[moduleLocation];
+    if (string === undefined) {
+      throw new ReferenceError(
+        `Cannot retrieve module at location ${q(moduleLocation)}.`
+      );
+    }
+    return string;
+  };
+};
+
+const makeImporter = (locate, retrieve) => async (moduleSpecifier) => {
+  const moduleLocation = locate(moduleSpecifier);
+  const string = await retrieve(moduleLocation);
+  return new StaticModuleRecord(string, moduleLocation, {
+    jsx: true,
+  });
+};
+
+const createVirtualModuleFromVariable = (name, exports, options = {}) => {
+  const { jsx } = options;
+  const exportKeys = Object.keys(exports);
+  const hasDefault = exportKeys.some((_export) => _export === "default");
+  const exportsList = exportKeys.filter((_export) => _export !== "default");
+  const declarations = exportsList.map((_, index) => `__v$${index}$__`);
+  const declarationString = declarations
+    .map((id, index) => `${id} = ${exportsList[index]}`)
+    .join(",");
+  const exportString = declarations
+    .map((id, index) => `${id} as ${exportsList[index]}`)
+    .join(",");
+
+  const moduleRecord = new StaticModuleRecord(
+    [
+      `const ${declarationString}`,
+      `export { ${exportString} }`,
+      hasDefault ? `export default __v_default__` : null,
+    ]
+      .filter(Boolean)
+      .join(";"),
+
+    name,
+    { jsx }
+  );
+
+  let globals = exports;
+  if (hasDefault) {
+    const { default: __v_default__, ...rest } = exports;
+    globals = { __v_default__, ...rest };
+  }
+
+  const compartment = new Compartment(
+    globals,
+    {},
+    {
+      name,
+      resolveHook: (spec) => spec,
+      importHook: (moduleSpecifier) => {
+        if (moduleSpecifier !== name) {
+          throw new ReferenceError(
+            `Cannot retrieve module at location ${q(moduleSpecifier)}.`
+          );
+        }
+        return moduleRecord;
+      },
+    }
+  );
+
+  return compartment.module(name);
+};
+
+const LayoutContext = React.createContext({
+  createOnRender: (originalCallback) => (info) => {
+    console.log(originalCallback, info);
+  },
+});
+
+const createDocumentWithCallback = (ReactPDF) =>
+  function DocumentWithCallback(props) {
+    const context = React.useContext(LayoutContext);
+
+    return React.createElement(ReactPDF.Document, {
+      ...props,
+      onRender: context.createOnRender(props.onRender),
+    });
+  };
+
+const Provider = (props) => {
+  const createOnRender = (originalCallback) => {
+    if (!originalCallback) return props.fn;
+
+    const handler = (info) => {
+      const { layout, ...other } = info;
+      props.fn({ layout });
+      originalCallback(other);
+    };
+
+    handler._originalFunction = originalCallback;
+
+    return handler;
+  };
+  return React.createElement(
+    LayoutContext.Provider,
+    { value: { createOnRender } },
+    props.children
+  );
+};
+
+const tailwindModule = createVirtualModuleFromVariable(
+  "react-pdf-tailwind",
+  tailwind
+);
+const reactModule = createVirtualModuleFromVariable("react", React);
+const reactRuntimeModule = createVirtualModuleFromVariable(
+  "react/jsx-runtime",
+  {
+    jsx,
+    jsxs,
+    Fragment,
+  }
+);
+
+const reactPdfModule = createVirtualModuleFromVariable("@react-pdf/renderer", {
+  ...rpGlobals,
+  Document: createDocumentWithCallback(rpGlobals),
+});
+
+const isRelative = (spec) =>
+  spec.startsWith("./") ||
+  spec.startsWith("../") ||
+  spec === "." ||
+  spec === "..";
+
+const locate = (moduleSpecifier) => moduleSpecifier;
+const resolveHook = (spec, referrer) => {
+  if (isRelative(spec)) {
+    return new URL(spec, referrer).toString();
+  }
+
+  return spec;
+};
+
+const fitInto = (string, size) => {
+  if (string.length <= size) return string;
+  const halfOfSize = Math.floor((size - 3) / 2);
+  return [
+    string.substring(0, halfOfSize),
+    string.substring(string.length - halfOfSize, string.length),
+  ].join("...");
+};
+
+const fnName = (fn) => fn.name ?? "anonymous function";
+
+const serializeProps = (props) => {
+  if (!props) return;
+
+  return Object.fromEntries(
+    Object.entries(props)
+      .map(([key, value]) => {
+        if (key === "onRender") {
+          return value._originalFunction
+            ? [key, fnName(value._originalFunction)]
+            : false;
+        }
+
+        if (value === null) return [key, "null"];
+        if (value === undefined) return [key, "undefined"];
+
+        if (Array.isArray(value)) return [key, `[${value.join(", ")}]`];
+
+        switch (typeof value) {
+          case "number":
+          case "boolean":
+            return [key, value.toString()];
+          case "string":
+            return [key, fitInto(value.trim(), 100)];
+          case "function":
+            return [key, fnName(value)];
+          default:
+            return [key, typeof value];
+        }
+      })
+      .filter(Boolean)
+  );
+};
+
+const serializeLayout = (layout) => {
+  const serializeNode = (node) => {
+    const sNode = {
+      box: node.box,
+      style: node.style,
+      type: node.type,
+      props: serializeProps(node.props),
+      lines: node?.lines?.map((line) => ({ string: line.string })),
+      children: [],
+    };
+
+    if (node.children) {
+      for (let child of node.children) {
+        sNode.children.push(serializeNode(child));
+      }
+    }
+
+    return sNode;
+  };
+
+  return layout && serializeNode(layout);
+};
+
+const evaluate = async (code) => {
+  try {
+    const retrieve = makeStaticRetriever({
+      "file://internal/code.js": code,
+    });
+    const importHook = makeImporter(locate, retrieve);
+
+    const compartment = new Compartment(
+      {
+        console,
+        fetch: (...a) => fetch(...a),
+        Headers,
+        Request,
+        Response,
+      },
+      {
+        react: reactModule,
+        "react/jsx-runtime": reactRuntimeModule,
+        "@react-pdf/renderer": reactPdfModule,
+        "react-pdf-tailwind": tailwindModule,
+      },
+      {
+        name: "repl",
+        resolveHook,
+        importHook,
+      }
+    );
+
+    const { namespace } = await compartment.import("file://internal/code.js");
+
+    if (!("default" in namespace)) {
+      throw new Error("The default export is not a React PDF Component");
+    }
+
+    let layout = null;
+
+    const element = React.createElement(
+      Provider,
+      { fn: (info) => (layout = info._INTERNAL__LAYOUT__DATA_) },
+      React.createElement(namespace.default)
+    );
+    const buffer = await rpGlobals.renderToBuffer(element);
+
+    return {
+      buffer,
+      layout: serializeLayout(layout),
+    };
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const NodeCanvasFactory = {
+  create(width, height) {
+    const canvas = createCanvas(width, height);
+    const context = canvas.getContext("2d");
+    return {
+      canvas,
+      context,
+    };
+  },
+
+  reset(canvasAndContext, width, height) {
+    canvasAndContext.canvas.width = width;
+    canvasAndContext.canvas.height = height;
+  },
+
+  destroy(canvasAndContext) {
+    canvasAndContext.canvas = null;
+    canvasAndContext.context = null;
+  },
+};
+
+async function getCanvas(pagePromise, { height, width }) {
+  const page = await pagePromise;
+  let viewport = page.getViewport({ scale: 1.0 });
+  viewport = page.getViewport({
+    scale: Math.max(height / viewport.height, width / viewport.width),
+  });
+
+  const canvasFactory = NodeCanvasFactory;
+  const { canvas, context } = canvasFactory.create(
+    viewport.width,
+    viewport.height
+  );
+  const renderContext = {
+    canvasContext: context,
+    viewport,
+    canvasFactory,
+  };
+
+  const renderTask = page.render(renderContext);
+  await renderTask.promise;
+
+  return { canvas, viewport };
+}
+
+export default async function GET(req, res) {
+  const { cp_code } = req.query;
+  const ctx = canvas.getContext("2d");
+  ctx.fillStyle = "white";
+  ctx.fillRect(0, 0, 1600, 900);
+  ctx.drawImage(await getTemplate(), 0, 0);
+
+  const pdf = await evaluate(cp_code ? decompress(cp_code) : code);
+
+  const document = await pdfjs.getDocument({
+    data: pdf.buffer,
+    verbosity: 0,
+  }).promise;
+
+  const { canvas: pdfCanvas, viewport } = await getCanvas(document.getPage(1), {
+    width: 700,
+    height: 900,
+  });
+  ctx.save();
+  ctx.fillStyle = "#e2e2e2";
+  ctx.shadowColor = "rgba(0, 0, 0, .2)";
+  ctx.shadowBlur = 200;
+  ctx.shadowOffsetY = 300;
+  ctx.fillRect(800 - 1, 100 - 1, viewport.width + 2, viewport.height + 2);
+  ctx.drawImage(pdfCanvas, 800, 100);
+  ctx.restore();
+
+  res.setHeader("Cache-Control", "public, max-age=2592000, immutable");
+  res.setHeader("content-type", "image/jpeg");
+  return res.send(canvas.toBuffer("image/jpeg"));
+}
