7fb4098ce2ad0f9f9df13b263c5e3637adfe345f
Dmitry Ivakhnenko <jeetiss@yandex.ru>
2023-03-16 14:34:16 +0300

    add more metadata

--- app/layout.js
+++ app/layout.js
@@ -2,17 +2,42 @@ import { Analytics } from "@vercel/analytics/react";
 
 import "./globals.css";
 
+/** @type {import('next').Metadata} */
 export const metadata = {
   title: "react-pdf playground",
   description: "react pdf playground with interactive debugger",
+  keywords: [
+    "PDF",
+    "@react-pdf/renderer",
+    "react-pdf",
+    "debugger",
+    "debug pdf",
+  ],
+  robots: "index, follow",
+  icons: "/favicon.svg",
+  openGraph: {
+    type: "website",
+    url: "https://react-pdf-repl.vercel.app/",
+    title: "react-pdf playground",
+    description: "react pdf playground with interactive debugger",
+    siteName: "react-pdf-repl",
+    images: [
+      {
+        url: "https://react-pdf-repl.vercel.app/og.png",
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    site: "https://react-pdf-repl.vercel.app/",
+    creator: "Dmitry Ivakhnenko <jeetiss@ya.ru>",
+    images: "https://react-pdf-repl.vercel.app/og.png",
+  },
 };
 
 export default function RootLayout(props) {
   return (
     <html lang="en">
-      <head>
-        <link rel="icon" type="image/svg" href="/favicon.svg" />
-      </head>
       <body>
         {props.children}
         <Analytics />
