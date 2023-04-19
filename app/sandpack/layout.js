607eb4de0a7171e92ca4cb3b23bd53d544ccc20e
Dmitry Ivakhnenko <jeetiss@yandex.ru>
2023-04-19 12:34:39 +0300

    add no index flag to  sandpack page

--- app/sandpack/layout.js
+++ app/sandpack/layout.js
@@ -1,8 +1,10 @@
 import { GlobalSandpackStyles } from "./global-styles";
 
+/** @type {import('next').Metadata} */
 export const metadata = {
   title: "Sandpack",
   description: "react-pdf/renderer with sandpack",
+  robots: "noindex, nofollow",
 };
 
 export default function RootLayout(props) {
--- app/sitemap.js
+++ app/sitemap.js
@@ -1,5 +1,5 @@
 export default async function sitemap() {
-  return ["", "/sandpack"].map((route) => ({
+  return [""].map((route) => ({
     url: `https://react-pdf-repl.vercel.app${route}`,
     lastModified: new Date().toISOString().split("T")[0],
   }));
